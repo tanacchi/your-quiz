47947bbd98ddd1e156fbdbc431c9cd8a6fd4c2cb
tanacchi <q111026d@mail.kyutech.jp>
2025-08-05 20:28:58 +0900

    Generate types for API I/F

--- api/spec/schema.sql
+++ api/spec/schema.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys = ON;
 
 -- SQL dump generated using DBML (dbml.dbdiagram.io)
 -- Database: PostgreSQL
--- Generated at: 2025-08-04T14:15:34.814Z
+-- Generated at: 2025-08-04T15:23:24.080Z
 
 CREATE TYPE "AnswerType" AS ENUM (
   'boolean',
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,33 @@
 import { Hono } from "hono";
+import type { paths } from "./types/generated/api";
+import { createHonoOpenApiRouter } from "openapi-ts-router";
+import z from "zod";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+// const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono();
+const api = createHonoOpenApiRouter<paths>(app);
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+api.post('/pets/:id', {
+  bodyValidator: z.object({ name: z.string() }),
+  handler: async c => {
+    const { name } = c.req.valid('json');
+    // ビジネスロジック呼び出し
+    const created = { id: 'u123', name };
+    return c.json(created);
+  },
+});
+
+// APIルートの実装（型安全性は型インポートで保証）
+api.get("/pet/{petId}", {
+  pathValidator: zValidator(
+    z.object({
+      petId: z.number(), // petIdが数値であることを検証
+    })
+  ),
+  handler: (c) => {
+    const { petId } = c.req.valid("param"); // 検証済みのパラメータにアクセス
+    return c.json({ name: "Falko", photoUrls: [] });
+  },
 });
 
 export default app;
