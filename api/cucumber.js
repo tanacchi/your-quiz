f1a5b0d8ed8a2f571c641f5a491e184907068f18
tanacchi <q111026d@mail.kyutech.jp>
2025-08-08 10:23:00 +0900

    Fix cucumber config

--- api/cucumber.js
+++ api/cucumber.js
@@ -1,18 +1,10 @@
 export default {
   default: {
-    require: [
-      "tests/support/hooks.ts",
-      "tests/support/world.ts",
-      "tests/steps/common-steps.ts",
-      "tests/steps/quiz-creation-steps.ts",
-      "tests/steps/quiz-retrieval-steps.ts",
-      "tests/steps/quiz-search-steps.ts",
-      "tests/steps/type-safety-steps.ts",
-      "tests/features/**.ts",
-    ],
+    require: ["tests/steps/**/*.ts"],
     requireModule: ["tsx"],
     format: ["pretty", "json:tests/reports/cucumber-report.json"],
-    paths: ["tests/features/**/*.feature"],
-    publishQuiet: true,
+    // paths: ["tests/features/**/*.feature"],
+    publishQuiet: false,
+    strict: true,
   },
 };
