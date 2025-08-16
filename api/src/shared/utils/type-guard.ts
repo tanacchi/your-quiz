f3a80c18d370a56e804087b44c5aedb3e2d1fcb3
tanacchi <q111026d@mail.kyutech.jp>
2025-08-17 02:26:02 +0900

    Add comments of isObjectLike

--- api/src/shared/utils/type-guard.ts
+++ api/src/shared/utils/type-guard.ts
@@ -1,3 +1,8 @@
+/**
+ * Checks if the input is an object-like value (i.e., not null and of type "object").
+ * @param input The value to check.
+ * @returns True if the input is object-like, false otherwise.
+ */
 export const isObjectLike = <T>(input: unknown): input is Partial<T> => {
   return typeof input === "object" && input !== null;
 };
